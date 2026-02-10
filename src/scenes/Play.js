c22997314926e08dbb387c6e4776a86f992adbf2
Max Kinet <mkinet@ucsc.edu>
2026-02-09 20:47:12 -0800

    add scrolling background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,10 +1,17 @@
+/* Infinite scrolling technique:
+    *  - The ball actually falls through game space so that it can take advantage of built-in physics
+    *  functionality like gravity and bounce.
+    *  - The background sprite is locked to the camera using 0 scrollfactor.
+    *  - The background is a scrolling tilesprite.
+    */ 
 class Play extends Phaser.Scene {
     constructor() {
         super('Play');
     }
 
     preload() {
-       this.load.image('old_ball', './assets/old_ball.png');
+        this.load.image('old_ball', './assets/old_ball.png');
+        this.load.image('sky', './assets/sky.png');
     }
 
     create() {
@@ -13,11 +20,14 @@ class Play extends Phaser.Scene {
             immovable: true,
         });
 
+        // Add background tilesprite
+        this.sky = this.add.tileSprite(0, 0, 480, 960, 'sky').setOrigin(0);
+        // Scroll factor is how much a game object is displaced when the camera moves.
+        this.sky.setScrollFactor(0);
         // Add ball sprite
         this.ball = this.physics.add.sprite(200, 100, 'old_ball');
         this.ball.setCircle(this.ball.width / 2);
         this.ball.setGravityY(200);
-        this.ball.body.setCollideWorldBounds(true);
         this.ball.body.setBounce(0.5);
         this.ball.body.setMaxVelocityY(MAX_VELOCITY);
 
@@ -26,7 +36,7 @@ class Play extends Phaser.Scene {
         this.obstacle = this.add.rectangle(
             Phaser.Math.Between(0, width - 50),
             height -200,
-            width,
+            size,
             10,
             0xFF0000
         ).setOrigin(0);
@@ -40,7 +50,8 @@ class Play extends Phaser.Scene {
     }
 
     update() {
-
+        // Scroll the background according to how much the camera has moved since game start
+        this.sky.tilePositionY = this.cameras.main.scrollY;
     }
 
 }
